f594ad3d4f5f5199d0c8c8c7f0dcee70af1cf998
Md Toufiquzzaman <150599497+runtimeninja@users.noreply.github.com>
2025-12-29 01:00:11 +0600

    feat: url resolution and counter increment

---
+++ api/routes/resolve.go
@@ -0,0 +1,36 @@
+package routes
+
+import (
+	"time"
+	"github.com/runtimeninja/golang-redis-url-shortner/database"
+	"github.com/redis/go-redis/v9"
+	"github.com/gofiber/fiber/v2"
+)
+
+func ResolveURL(c *fiber.Ctx) error {
+	url := c.Params("url")
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	// local timeout context for Redis operations
+	timeoutCtx, cancel := time.WithTimeout(database.Ctx, 3*time.Second)
+	defer cancel()
+
+	value, err := r.Get(timeoutCtx, url).Result()
+	if err == redis.Nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "short not found in database",
+		})
+	} else if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "cannot connect to database",
+		})
+	}
+
+	if err := r.Incr(timeoutCtx, "counter").Err(); err != nil {
+		log.Printf("failed to increment counter: %v", err)
+	}
+
+	return c.Redirect(value, 301)
+}
